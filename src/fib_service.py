9d16a09933c238ba46ea723ea316005423d9fef9
longchunzhang <longchunzhang@sina.com>
2016-03-21 13:54:17 +0800

    First version, including basic service function.

---
+++ src/fib_service.py
@@ -0,0 +1,42 @@
+#!/root/emc_test/restapi_test/venv/bin/python
+from flask import Flask, jsonify, make_response
+import string
+
+app = Flask(__name__)
+
+def fibonacci(n):
+	if n == 0:
+		return [0]
+	if n == 1:
+		return [1]
+	if n == 2:
+		return [1, 1]
+	
+	fibs = [0, 1]
+	for i in range(2, n):
+		fibs.append(fibs[-1] + fibs[-2])
+	return fibs
+
+
+@app.route('/testservices/api/v1.0/fibonacci/<fib_n>', methods=['GET'])
+def get_fibonacci(fib_n):
+	try:
+		fib_n_num = string.atoi(fib_n)
+	except ValueError:
+		return jsonify({'error': fib_n + ' is not a valid integer'})
+	
+	if fib_n_num > 10000:
+		return jsonify({'error': fib_n + ' is larger than 10000'})
+	if fib_n_num <= 0:
+		return jsonify({'error': fib_n + ' is less than 1'})
+
+	fib_array = fibonacci(fib_n_num)
+	return jsonify({'fibonacci': fib_array})
+
+
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+	app.run(debug=True)
